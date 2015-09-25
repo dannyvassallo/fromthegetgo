e13e600dee2d4dc9627509b46a0ebb83467e432e
dannyvassallo <danielvassallo87@gmail.com>
2015-09-25 16:52:02 -0400

    added social feed

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,4 +1,7 @@
 //= require jquery
 //= require bootstrap
 //= require _jquery.fitvids
+//= vendor/__jquery.social.stream.1.5.9
+//= vendor/_jquery.social.stream.wall.1.6
+//= social-wall
 //= require_tree .
